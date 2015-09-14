454f0aa70559913624d6b2ebfc2e1ba19d5bf5a6
sandinosaso <sandinosaso@gmail.com>
2015-09-14 16:08:36 -0300

    Root password for test environment

--- environments/test/common/config/main-local.php
+++ environments/test/common/config/main-local.php
@@ -5,7 +5,7 @@ return [
             'class' => 'yii\db\Connection',
             'dsn' => 'mysql:host=localhost;dbname=demo_test',
             'username' => 'root',
-            'password' => '',
+            'password' => 'root',
             'charset' => 'utf8',
         ], 
         'mailer' => [
